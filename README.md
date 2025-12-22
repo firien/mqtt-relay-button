33219da9413030fce24ed3c8a4eda16d012339cb
Patrick Griffin <58729+firien@users.noreply.github.com>
2025-12-22 12:40:15 -0500

    restart

---
+++ README.md
@@ -0,0 +1,14 @@
+PI Zero 2 W with [Relay HAT](https://github.com/sbcshop/Relay-4-Zero) 
+
+Relies on `.env` file with defined variables:
+
+* `USERNAME`
+* `PASSWORD`
+* `CLIENTID`
+* `DEVICEID`
+* `HAPORT`
+* `HAHOST`
+
+One 
+
+export PATH=$PATH:$HOME/node-v24.12.0-linux-arm64/bin
--- mqtt.js
+++ mqtt.js
@@ -106,50 +106,61 @@ const connectionPacket = await connection().bytes()
 const discoveryPacket = await discovery().bytes()
 const subscribePacket = await subscribe().bytes()
 
-const openSocket = () => {
-  return new Promise((resolve, reject) => {
-    const port = Number(process.env.HAPORT) 
-    const socket = createConnection(port, process.env.HAHOST, () => {
-      console.log('connected to server!')
-      socket.setKeepAlive(true) 
-      resolve(socket)
+const discoveryAndSubscribe = async () => {
+
+  const openSocket = () => {
+    return new Promise((resolve, reject) => {
+      const port = Number(process.env.HAPORT) 
+      const socket = createConnection(port, process.env.HAHOST, () => {
+        console.log('connected to server!')
+        socket.setKeepAlive(true) 
+        resolve(socket)
+      })
     })
-  })
-}
-const socket = await openSocket()
-socket.on('error', (data) => {
-  console.log(data)
-})
-
-socket.on('end', () => {
-  console.log('all things must end')
-  //#socket.connect()
-})
-
-socket.on('data', (data) => {
-  console.log(data)
-  if (data[0] === 0x30) {
-    console.log('trigger relay!')
-    rpio.write(pin, rpio.HIGH)
-    setTimeout(() => {
-      rpio.write(pin, rpio.LOW)
-    }, 500)
   }
-});
-socket.write(connectionPacket)
-// console.log(await discovery().arrayBuffer())
-setTimeout(() => {
-  console.debug('writing discovery')
-  socket.write(discoveryPacket)
-}, 300)
+  const socket = await openSocket()
+  socket.on('error', (data) => {
+    console.error(data)
+    socket.destroy()
+  })
 
-setTimeout(() => {
-  console.debug('writing subscribe')
-  socket.write(subscribePacket)
-}, 600)
+  socket.on('close', () => {
+    console.debug('closing')
+    clearInterval(heartbeat)
+    // restart
+    setTimeout(discoveryAndSubscribe, 1000)
+  })
 
-const heartbeat = new Uint8Array([0xC0, 0])
+  socket.on('end', () => {
+    console.debug('ended')
+    socket.destroy()
+  })
 
-setInterval(() => {
-  socket.write(heartbeat)
-}, 15000)
+  socket.on('data', (data) => {
+    if (data[0] === 0x30) {
+      console.log('trigger relay!')
+      rpio.write(pin, rpio.HIGH)
+      setTimeout(() => {
+        rpio.write(pin, rpio.LOW)
+      }, 500)
+    }
+  })
+  socket.write(connectionPacket)
+
+  setTimeout(() => {
+    console.debug('writing discovery')
+    socket.write(discoveryPacket)
+  }, 300)
+
+  setTimeout(() => {
+    console.debug('writing subscribe')
+    socket.write(subscribePacket)
+  }, 600)
+
+  const ping = new Uint8Array([0xC0, 0])
+  const heartbeat = setInterval(() => {
+    socket.write(ping)
+  }, 15000)
+}
+
+discoveryAndSubscribe()
